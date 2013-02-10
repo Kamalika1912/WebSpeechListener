d24cc4e19b52727d3f14c994f1d18547db35bf92
Sandeep S Kumar <sanygeek@gmail.com>
2013-02-10 14:21:57 +0530

    Basic support for command parsing using switch cases.

---
+++ logic.js
@@ -0,0 +1,106 @@
+(function(){
+	var box = document.getElementById("box");
+})()
+
+function color (clr) {
+	box.style.backgroundColor = clr;
+}
+
+function move (dir) {
+	switch(dir) {
+		case "right"		: {
+			box.style.left = "50px";
+			break;
+		}
+		case "left"			: {
+			box.style.right = "50px";
+			break;	
+		}
+		case "top"			: {
+			box.style.bottom = "50px";
+			break;
+		}
+		case "bottom"		: {
+			box.style.top = "50px";
+			break;
+		}
+	}
+
+	box.style[dir] = "0px";
+}
+
+function shape (shap) {
+	if(shap === "square") {
+		box.style.borderRadius = "0%";
+	}
+
+	else if(shap === "circle") {
+		box.style.borderRadius = "50%";	
+	}
+}
+
+function sayThis(str){
+	//document.getElementById("text").innerHTML = "Command: " + str;
+	console.log("Command: " + str);
+	var words = str.split(" ");
+	words.forEach(function(x){
+		switch(x) {
+			case "red"		:
+			case "green"	:
+			case "blue"		:
+			case "yellow"	:
+			case "brown"	:
+			case "orange"	:
+			case "cyan"		:
+			case "gray"		:
+			case "pink"		:
+			case "purple"	:
+			case "violet"	:
+			case "yellow"	:
+			case "black"	:
+			case "white"	: {
+				color(x);
+				break;
+			}
+
+			case "right"	: {
+				move("right");
+				break;
+			}
+			case "left"		: {
+				move("left");
+				break;
+			}
+			
+			case "up"		:
+			case "top"		: {
+				move("top");
+				break;
+			}
+
+			case "down"		:
+			case "bottom"	: {
+				move("bottom");
+				break;
+			}
+
+			case "circle"	:
+			case "circles"	:
+			case "round"	: {
+				shape("circle");
+				break;
+			}
+
+			case "box"		:
+			case "square"	: {
+				shape("square");
+				break;
+			}
+
+			default			: {
+				console.log("Sorry, '" + x + "' was not recognised as a command!");
+			}
+		}
+
+	});
+}
