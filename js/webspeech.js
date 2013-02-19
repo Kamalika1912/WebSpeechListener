971b7fc698358616a1e8eb7f0a535ee7a1ff6003
Sandeep S Kumar <sanygeek@gmail.com>
2013-02-19 17:21:19 +0530

    Refined implementation to show interim results.

--- js/webspeech.js
+++ js/webspeech.js
@@ -1,11 +1,13 @@
 var speechRecog = new window.webkitSpeechRecognition;
 var recognizing = false;
 var speechResult = '';
+var interResult = '';
 
 //console.log("JS test [Running]");
 
 speechRecog.continuous = true;
-speechRecog.interimResults = false;
+speechRecog.interimResults = true;
+speechRecog.maxAlternatives = 3;
 
 speechRecog.onstart = function () {
     $(".mic").css("backgroundColor", "red");
@@ -13,15 +15,48 @@ speechRecog.onstart = function () {
     recognizing = true;
 }
 
-speechRecog.onresult = function (event) {
-    speechResult = event.results[0][0].transcript;
-    conf = event.results[0][0].confidence;
-    confidence.innerHTML = conf;
-    result.innerHTML = speechResult;
-    console.log(speechResult);
-    speechRecog.stop();
+speechRecog.onaudiostart = function () {
+    console.log("Audio Started...");
+}
+
+speechRecog.onaudioend = function () {
+    console.log("Audio Ended...");
     console.log("Stopped recording.");
     recognizing = false;
+}
+
+speechRecog.onspeechstart = function () {
+    console.log("Speech Started...");
+}
+
+speechRecog.onspeechend = function () {
+    console.log("Speech Ended...");
+}
+
+speechRecog.onsoundstart = function () {
+    console.log("Sound Started...");
+}
+
+speechRecog.onsoundend = function () {
+    console.log("Sound Ended...");
+}
+
+speechRecog.onresult = function (event) {
+    for (var i = event.resultIndex; i < event.results.length; i += 1) {
+        if (!event.results[i].isFinal) {
+            interResult = event.results[i][0].transcript;
+            console.log("Intermediate Result: " + interResult);
+        } else {
+            speechResult = event.results[i][0].transcript;
+        }
+    }
+    conf = event.results[0][0].confidence.toPrecision(2) * 100 + '%';
+    confidence.innerHTML = conf;
+    result.innerHTML = speechResult;
+    //console.log(speechResult);
+    //speechRecog.stop();
+    //console.log("Stopped recording.");
+    //recognizing = false;
 }
 
 speechRecog.onend = function () {
